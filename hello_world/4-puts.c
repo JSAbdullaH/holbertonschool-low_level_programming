efb4b1007e3e175af9908e6b3217afb2d7452650
JSAbdullaH <dooy-20-10@hotmail.com>
2026-02-23 20:14:51 +0000

    t

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-    
+	puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }

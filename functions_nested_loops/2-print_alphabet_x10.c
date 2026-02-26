ad741c41cd151e857cfa66b505f7c29bbada2456
Abdullah Mohammed <abdullah@Abdullahs-MacBook-Air.local>
2026-02-26 22:25:05 +0300

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10  - Entry point
+ * print_alphabet_x10  - prints the alphabet 10 times
  *
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-
+    int i=0;
 	char c;
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);

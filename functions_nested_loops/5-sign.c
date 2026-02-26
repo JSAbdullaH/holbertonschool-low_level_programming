7636ca24cf824f3c9672dd2f05a3f65d51353fc4
Abdullah Mohammed <abdullah@Abdullahs-MacBook-Air.local>
2026-02-26 22:43:17 +0300

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int print_sign  - print the char is alphabet or not
+ *
+ * @c: the char to check
+ * Return: 1 if c alphabet , 0 othgrwise
+ */
+int print_sign(int n)
+{
+    if (n > 0)
+    return(1);
+    else if (n = 0)
+    return(0);
+    else
+    return(-1);
+}

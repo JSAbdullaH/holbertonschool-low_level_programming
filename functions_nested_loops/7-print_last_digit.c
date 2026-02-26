29afc8a2e6b232233a2cafa2367c3890cf6d4650
Abdullah Mohammed <abdullah@Abdullahs-MacBook-Air.local>
2026-02-26 23:00:08 +0300

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_last_digit  - print las digit of any number
+ *
+ * @n: the int to check
+ * Return: l
+ */
+int print_last_digit(int n)
+{
+    int l = n % 10;
+    return (l);
+
+}

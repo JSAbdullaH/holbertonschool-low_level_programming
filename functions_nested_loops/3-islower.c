74dd96fbc0baa844c479df99ce75f69a4c596cb3
Abdullah Mohammed <abdullah@Abdullahs-MacBook-Air.local>
2026-02-26 22:34:45 +0300

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower  - prints the alphabet 10 times
+ *
+ * Return: 1 if c lower case , 0 othgrwise
+ */
+int _islower(int c)
+{
+    if(c >= 'a' && c <= 'z')
+    return(1);
+    else
+    return(0);
+}

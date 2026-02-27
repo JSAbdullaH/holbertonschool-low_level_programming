9513cd5af0634f08b8e10d2e9b4a8e6f2ef43274
JSAbdullaH <doody-20-10@hotmail.com>
2026-02-27 22:51:46 +0300

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_last_digit  - print las digit of any number
+ *
+ * @a: the firs int to add
+ * @b: sec int to add
+ * Return: res
+ */
+int add(int a, int b)
+{
+    int res = a + b;
+    _putchar('0' + res);
+    _putchar('\n');
+    return(res);
+}

eb346bebfc65ccf03ec0f795bc56395daa07d225
JSAbdullaH <doody-20-10@hotmail.com>
2026-02-27 22:40:53 +0300

    8&9

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <time.h>
+
+/**
+ *jack_bauer  - print the time 
+ *
+ * @h: hours
+ * @m: minutes
+ * Return: void no return 
+ */
+void jack_bauer(void)
+{
+    int h , m;
+
+    for (h = 0; h < 24; h++)
+    {
+        for (m = 0; m < 60; m++)
+        {
+			_putchar('0' + h / 10);
+			_putchar('0' + h % 10);
+			_putchar(':');
+			_putchar('0' + m / 10);
+			_putchar('0' + m % 10);
+			_putchar('\n');
+        }
+        
+    }
+    
+}

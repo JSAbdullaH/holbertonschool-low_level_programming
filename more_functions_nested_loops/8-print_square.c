40b4ac5ee02ea098c4f7648e081a189d88865adb
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-01 19:08:55 +0000

    8 Fix

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,16 +8,17 @@
  */
 void print_square(int size)
 {
-	int i;
-
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		int j;
+		_putchar('\n')
+			return;
+	}
+	int j;
 
-		for (j = 0; j < size; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
+	for (j = 0; j < size; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
 	}
 }

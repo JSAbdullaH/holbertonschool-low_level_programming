5f6092ba15a00d27c5cb15264b66473bf652ff6f
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-01 20:53:30 +0300

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - get char and check its upper or not
+ *
+ *Return: 1 if its upper , 0 otherwise
+ */
+int _isupper(int c)
+{
+	int c;
+    if(c >= 'A' || c <= 'Z')
+    return (1);
+    else
+    return(0);
+
+}

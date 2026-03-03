6801103c00fb87ff66672f12ca7f7ff50eb202d7
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-03 19:20:30 +0000

    7 Fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	if (length % 2 == 0)
 		start = length / 2;
 	else
-		start = (length - 1) / 2;
+		start = (length + 1) / 2;
 
 	for (i = start; str[i] != '\0'; i++)
 		_putchar(str[i]);

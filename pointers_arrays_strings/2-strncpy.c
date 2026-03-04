931423d6380a50edc19d263daae7174c97f61264
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-04 19:00:08 +0000

    2-

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
- *_strcat - concatenates two strings
+ *_strncpy - concatenates two strings
  *
  *@dest: pointer to destination string
  *@src: pointer to source string
+ *@n: the int to copy
  *Return: pointer to the resulting string
  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	 int i;
 
- char *_strncpy(char *dest, char *src, int n)
- {
-    int i;
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    dest[i] = src[i];
-    for (; i < n; i++)
-    dest[i] = '\0';
-    return (dest);
- }
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}

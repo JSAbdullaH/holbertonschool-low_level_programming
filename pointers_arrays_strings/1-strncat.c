33ae9db6b31c35e8c15e96ec759fb862536bcce4
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-04 18:51:18 +0000

    -1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,27 @@
 #include "main.h"
+
 /**
- *_strcat - concatenates two strings
+ *_strncat - concatenates two strings
  *
  *@dest: pointer to destination string
  *@src: pointer to source string
+ *@n: int to calc the words
  *Return: pointer to the resulting string
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, j;
-    i = 0;
-    while (dest[i] != '\0')
-        i++;
-    j = 0;
-    while (src[j] != '\0' && j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }

79750a201c8196794bd441de6bb93cc1b2eb5c0d
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-04 21:43:35 +0300

    0& main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_h
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -15,4 +15,12 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
 #endif

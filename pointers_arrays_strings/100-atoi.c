404d00093c28450068fd744e3b356bc47cbed834
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-04 18:17:32 +0000

    100 Fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,32 +9,32 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    long result = 0;
-    int found_digit = 0;
+	int sign = 1;
+	long result = 0;
+	int found_digit = 0;
 
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
 
-        if (*s >= '0' && *s <= '9')
-        {
-            found_digit = 1;
-            result = result * 10 + (*s - '0');
+		if (*s >= '0' && *s <= '9')
+		{
+			found_digit = 1;
+			result = result * 10 + (*s - '0');
 
-            if (result * sign > INT_MAX)
-                return (INT_MAX);
-            if (result * sign < INT_MIN)
-                return (INT_MIN);
-        }
-        else if (found_digit)
-            break;
-        s++;
-    }
+			if (result * sign > INT_MAX)
+				return (INT_MAX);
+			if (result * sign < INT_MIN)
+				return (INT_MIN);
+		}
+		else if (found_digit)
+			break;
+		s++;
+	}
 
-    if (!found_digit)
-        return (0);
+	if (!found_digit)
+		return (0);
 
-    return ((int)(result * sign));
+	return ((int)(result * sign));
 }

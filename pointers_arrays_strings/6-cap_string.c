bde61a2f1baf81a37a637adeb96ee160794d98c0
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-04 20:48:12 +0000

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,22 +6,21 @@
  *@str: pointer to the string
  *Return: pointer to the resulting string
  */
-
 char *cap_string(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
-            str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
-            str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
-            str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-            str[i - 1] == '{' || str[i - 1] == '}')
-        {
-            if (str[i] >= 'a' && str[i] <= 'z')
-                str[i] = str[i] - ('a' - 'A');
-        }
-    }
-    return (str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+				str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
+				str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
+				str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+				str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - ('a' - 'A');
+		}
+	}
+	return (str);
 }

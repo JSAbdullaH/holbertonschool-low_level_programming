40b212f577919db30e5e142466badacc52d9a706
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-04 20:44:29 +0000

    7 fix

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,21 +6,21 @@
  *Return: pointer to the resulting string
  */
 char *leet(char *str)
-{ 
-    int i, j;
-    char *leet_chars = "aAeEoOtTlL";
-    char *leet_replacements = "4433007711";
+{
+	int i, j;
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_replacements = "4433007711";
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; leet_chars[j] != '\0'; j++)
-        {
-            if (str[i] == leet_chars[j])
-            {
-                str[i] = leet_replacements[j];
-                break;
-            }
-        }
-    }
-    return (str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leet_chars[j] != '\0'; j++)
+		{
+			if (str[i] == leet_chars[j])
+			{
+				str[i] = leet_replacements[j];
+				break;
+			}
+		}
+	}
+	return (str);
 }

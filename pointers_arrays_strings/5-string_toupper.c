959cedfa37794f7fe08e6f49e617532bc2d3ff46
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-04 20:42:09 +0000

    6

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- *_string_toupper - converts a string to uppercase
+ *string_toupper - converts a string to uppercase
  *
  *@s: pointer to the string
  *Return: pointer to the resulting string
  */
 char *string_toupper(char *s)
 {
-    int i = 0;
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] >= 'a' && s[i] <= 'z')
-        s[i] = s[i] - 32;
-    }
-    return (s);
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
 }
--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,29 @@
 #include "main.h"
+
 /**
  *cap_string - capitalizes the first letter of each word in a string
  *
  *@str: pointer to the string
  *Return: pointer to the resulting string
  */
-
 char *cap_string(char *str)
 {
-    while (*str)
-    {
-        if (*str >= 'a' && *str <= 'z')
-        {
-            if (*(str - 1) == ' ' || *(str - 1) == '\t' ||
-                *(str - 1) == '\n' || *(str - 1) == ',' || *(str - 1) == ';' ||
-                *(str - 1) == '.' || *(str - 1) == '!' || *(str - 1) == '?' ||
-                *(str - 1) == '"' || *(str - 1) == '(' || *(str - 1) == ')' ||
-                *(str - 1) == '{' || *(str - 1) == '}')
-            {
-                *str = *str - ('a' - 'A');
-            }
-        }
-        str++;
-    }
-    return (str);
+	while (*str)
+	{
+		if (*str >= 'a' && *str <= 'z')
+		{
+			if (*(str - 1) == ' '  || *(str - 1) == '\t' ||
+					*(str - 1) == '\n' || *(str - 1) == ','  ||
+					*(str - 1) == ';'  || *(str - 1) == '.'  ||
+					*(str - 1) == '!'  || *(str - 1) == '?'  ||
+					*(str - 1) == '"'  || *(str - 1) == '('  ||
+					*(str - 1) == ')'  || *(str - 1) == '{'  ||
+					*(str - 1) == '}')
+			{
+				*str = *str - ('a' - 'A');
+			}
+		}
+		str++;
+	}
+	return (str);
 }

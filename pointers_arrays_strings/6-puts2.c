8fa136189dd35ad78ffbe2d14675b51b75e45a35
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-04 21:25:04 +0300

    fix 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -13,10 +13,8 @@ void puts2(char *str)
 	if (str == NULL)
 		return;
 
-	for (i = 0; str[i] != '\0'; i = i +2)
-	{
-		if (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i += 2)
 		_putchar(str[i]);
-		}
+
 	_putchar('\n');
 }

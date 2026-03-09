11a2f2b4a87931134bb13b960d3e424c03dd11fe
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-09 22:28:02 +0300

    all

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to memory area
+ * @b: byte to fill with
+ * @n: number of bytes to fill
+ * Return: pointer to memory area s
+ */

d9dfa26c4ebf3cdb92f8208493b4ae98e3312a4e
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-10 22:18:24 +0300

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,9 +7,13 @@
  */
 int _sqrt_recursion(int n)
 {
-if (n == 0)
+if (n < 0)
 {
-return 0;
+return (-1);
 }
-return _sqrt_recursion(n - 1);
+if (n == 0 || n == 1)
+{
+return (n);
+}
+return _sqrt_helper(n, 1);
 }

c1b378071043590b19cc3e2aba3ded360d94d88a
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-10 17:32:49 +0000

    ds

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,17 +4,17 @@
  * _strchr - locates a character in a string
  * @s: pointer to string
  * @c: character to locate
- * Return: pointer to first occurrence of character c in string s, or NULL if not found
+ * Return: pointer to first occurrence  or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-        {
-            return s;
-        }
-        s++;
-    }
-    return NULL;
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
 }

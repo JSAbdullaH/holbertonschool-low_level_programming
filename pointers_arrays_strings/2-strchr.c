97cfd6bc95133700c63ad13e5d358a9013d1833b
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-10 20:37:41 +0300

    1

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
-	return (NULL);
+    while (*s != '\0')
+    {
+        if (*s == c)
+            return (s);
+        s++;
+    }
+    /* handles the case where c == '\0' */
+    if (*s == c)
+        return (s);
+    return (NULL);
 }

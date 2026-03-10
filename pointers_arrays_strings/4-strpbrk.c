98a2788ebc8cde92765b07e4857092d7bd601d43
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-10 20:45:22 +0300

    4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurrence of any character from a set in a string
+ * @s: pointer to string
+ * @accept: pointer to accepted characters
+ * Return: pointer to the first occurrence of any character from the set, or NULL if not found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i, j;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+return (s + i);
+}
+}
+return (NULL);
+}

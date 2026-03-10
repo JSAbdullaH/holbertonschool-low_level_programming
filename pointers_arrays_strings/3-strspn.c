3113b5564f6524b95e106ca971beef512598972e
JSAbdullaH <doody-20-10@hotmail.com>
2026-03-10 20:56:25 +0300

    Fix

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,26 +8,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j;
-unsigned int count = 0;
+    unsigned int i, j;
+    unsigned int count = 0;
+    
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        int found = 0;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-int found = 0;
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                found = 1;
+                break;
+            }
+        }
 
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (s[i] == accept[j])
-{
-found = 1;
-break;
-}
-}
-
-if (found)
-count++;
-else
-break;
-}
-return (count);
+        if (found)
+            count++;
+        else
+            break;
+    }
+    return (count);
 }
